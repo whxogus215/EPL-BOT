f61dfa12478d25af7b4617b9fd66e9dea66dcba6
whxogus215 <whxogus215@naver.com>
2022-10-03 05:34:51 +0000

    FEAT show Club Table Max 5

--- epl-bot/app.js
+++ epl-bot/app.js
@@ -19,8 +19,8 @@ app.use('/api', apiRouter);
 
 function templateCarousel(data){
 
-  const score_obj = data["score"]["full_time"];
-  const score_arr = Object.values(score_obj);
+  const score_obj = data["score"]["full_time"]; // data의 score에 해당하는 오브젝트를 담은 뒤
+  const score_arr = Object.values(score_obj); // 담은 오브젝트의 value를 배열로 담는다.
   const roundNum = data["round"];
 
   const carousel = {
@@ -59,6 +59,15 @@ function templateCarousel(data){
   return carousel;
 }
 
+function tableCarousel(data){
+  const carousel = {
+    "title": `${data["rank"]}위 ${data["team_name"]}`,
+    "description": `${data["games_played"]}경기 ${data["points"]}승점 ${data["won"]}승 ${data["draw"]}무 ${data["lost"]}패 ${data["goals_for"]}득점 ${data["goals_against"]}실점 ${data["goals_diff"]}득실차`,
+  }
+
+  return carousel;
+}
+
 apiRouter.post('/sayHello', function(req, res) {
   
   // params에 해당하는 부분만 JSON 형태이므로 일반 객체로 변환
@@ -74,6 +83,11 @@ apiRouter.post('/result', (req,res)=>{
 
   const result = JSON.parse(req.body.action.params.round_num); // 라운드 입력 값
   const round_num = result.amount;
+  const simpleText = {
+    "simpleText" : {
+      "text" : `22-23 EPL ${round_num} 라운드 결과입니다.`
+    }
+  }; // 라운드 입력 값을 전달받아 simpleText(응답 시 첫 메세지)에 저장
 
   const options = {
     method: 'GET',
@@ -98,11 +112,7 @@ apiRouter.post('/result', (req,res)=>{
       "version": "2.0",
       "template": {
         "outputs": [
-          {
-              "simpleText" : {
-                "text" : `22-23 EPL ${this.roundNum} 라운드 결과입니다.`
-              }
-          },
+          simpleText,
           {
             "carousel": {
               "type" : "itemCard",
@@ -117,16 +127,65 @@ apiRouter.post('/result', (req,res)=>{
     const result = JSON.parse(body);
     result.data.forEach( a => {
       itemArr.push(templateCarousel(a));
-      // console.log(a)
     });
-    // console.log(result.data[1]);
-    
-    // console.log(dataTemplate);
     res.json(dataTemplate);
-
-    // 템플릿 반환하는 함수를 responseBody에 담고, 이를 res.send로 전달 / body 값을 파싱한 result (배열) -> 특정 라운드만 추출 -> 
   });
+})
 
+apiRouter.post('/team-table', (req, res)=>{
+
+  const options = {
+    method: 'GET',
+    url: 'https://livescore-football.p.rapidapi.com/soccer/league-table',
+    qs: {country_code: 'england', league_code: 'premier-league'},
+    headers: {
+      'X-RapidAPI-Key': process.env.API_KEY,
+      'X-RapidAPI-Host': 'livescore-football.p.rapidapi.com',
+      useQueryString: true
+    }
+  };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    const tableTemplate = {
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "carousel": {
+              "type" : "listCard",
+              "items" : [
+                {
+                  "header": {
+                    "title" : "EPL 22-23 팀 순위"
+                  },
+                  "items": []
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+    const itemArr = tableTemplate.template.outputs[0].carousel.items[0].items;
+    // // items에 각각의 팀 결과를 오브젝트로 담아서 추가해야 함!
+
+    const result = JSON.parse(body);
+    const table_data = result.data.total;
+
+    for (i = 0; i< 5; i++){
+      itemArr.push(tableCarousel(table_data[i]));
+    }
+
+    // table_data.forEach(a => {
+    //   itemArr.push(tableCarousel(a));
+    //     // 배열의 오브젝트 값을 함수로 넘김
+    // })
+    // console.log(itemArr);
+    // JSON.stringify(tableTemplate.template.outputs[0].carousel.items[0]);
+    res.json(tableTemplate);
+  });
 
 })
 
